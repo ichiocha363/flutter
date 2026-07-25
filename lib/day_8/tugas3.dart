02c8ee6ceaa1e344480ab6731df25ef17f727a52
zhilly <zhillyhilmansyah@gmail.com>
2026-07-26 00:53:11 +0700

    final tugas3

--- lib/day_8/tugas3.dart
+++ lib/day_8/tugas3.dart
@@ -11,83 +11,247 @@ class Tgs3Flutter extends StatelessWidget {
         title: Text("Registrasi & Edukasi"),
         centerTitle: true,
       ),
+      backgroundColor: Colors.grey[150],
 
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Align(alignment: Alignment.center),
-              Text(
-                "Form Registrasi",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Align(alignment: Alignment.center),
+                      Text(
+                        "Form Registrasi",
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 70,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.person),
+                            hintText: "Nama Pengguna",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 70,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.email),
+                            hintText: "Email Pengguna",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 70,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.phone),
+                            hintText: "Telephone Pengguna",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 70,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.key),
+                            hintText: "Input Password",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 70,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            suffixIcon: Icon(Icons.remove_red_eye),
+                            labelText: "Konfirmasi Password",
+                            hintText: "Masukan Konfirmasi Password",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
+              Text("Rekomendasi Tempat Wisata",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold),),
 
-              SizedBox(
-                height: 70,
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.person),
-                    hintText: "Nama Pengguna",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+              GridView.count(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+                crossAxisCount: 3,
+                children: <Widget>[
+                  Stack(
+                      alignment: AlignmentGeometry.bottomCenter,
+                    children: [
+                      Container(
+                        height: 300,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                            image: AssetImage('assets/image/jakarta.jpeg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 20,
+                        width: 100,
+                        color: Colors.white,
+                        child: Text(
+                          "Jakarta",
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    ],
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 70,
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email),
-                    hintText: "Email Pengguna",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                ),
-              ),
 
-              SizedBox(
-                height: 70,
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.phone),
-                    hintText: "Telephone Pengguna",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+                   Stack(
+                      alignment: AlignmentGeometry.bottomCenter,
+                    children: [
+                      Container(
+                        height: 300,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage('assets/image/bandung.jpeg'),
+                          fit: BoxFit.cover,),),),
+                      Container(
+                        height: 20,
+                        width: 100,
+                        color: Colors.white,
+                        child: Text(
+                          "Bandung",
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    ],
                   ),
-                ),
-              ),
 
-              SizedBox(
-                height: 70,
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.key),
-                    hintText: "Input Password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+                   Stack(
+                      alignment: AlignmentGeometry.bottomCenter,
+                    children: [
+                      Container(
+                        height: 300,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage('assets/image/padang.jpeg'),
+                          fit: BoxFit.cover,),),),
+                      Container(
+                        height: 20,
+                        width: 100,
+                        color: Colors.white,
+                        child: Text(
+                          "Padang",
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    ],
                   ),
-                ),
-              ),
 
-              SizedBox(
-                height: 70,
-                child: TextField(
-                  decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.remove_red_eye),
-                    labelText: "Konfirmasi Password",
-                    hintText: "Masukan Konfirmasi Password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+                   Stack(
+                      alignment: AlignmentGeometry.bottomCenter,
+                    children: [
+                      Container(
+                        height: 300,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage('assets/image/tokyo.jpeg'),
+                          fit: BoxFit.cover,),),),
+                      Container(
+                        height: 20,
+                        width: 100,
+                        color: Colors.white,
+                        child: Text(
+                          "Tokyo",
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    ],
                   ),
-                ),
+
+                   Stack(
+                      alignment: AlignmentGeometry.bottomCenter,
+                    children: [
+                      Container(
+                        height: 300,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage('assets/image/kyoto.jpeg'),
+                          fit: BoxFit.cover,),),),
+                      Container(
+                        height: 20,
+                        width: 100,
+                        color: Colors.white,
+                        child: Text(
+                          "Kyoto",
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    ],
+                  ),
+
+                   Stack(
+                      alignment: AlignmentGeometry.bottomCenter,
+                    children: [
+                      Container(
+                        height: 300,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage('assets/image/osaka.jpeg'),
+                          fit: BoxFit.cover,),),),
+                      Container(
+                        height: 20,
+                        width: 100,
+                        color: Colors.white,
+                        child: Text(
+                          "Osaka",
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
               ),
-            ],
+            ]
           ),
         ),
       ),
