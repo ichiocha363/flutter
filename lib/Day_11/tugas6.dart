06a38f9a0d9374ef84a33f4134b777a6b692dbd7
zhilly <zhillyhilmansyah@gmail.com>
2026-07-30 01:15:12 +0700

    tugas 6 Day11

--- lib/Day_11/tugas6.dart
+++ lib/Day_11/tugas6.dart
@@ -6,33 +6,174 @@ class Tugas6Day11 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
+        clipBehavior: Clip.none,
         children: [
-          Stack(
-            children: [
-              Container(
-                height: 450,
-                width: 450,
-                decoration: BoxDecoration(
-                  color: const Color.fromRGBO(37, 103, 232, 1),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [Text("Sign in to your")],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 190, top: 70),
-                height: 40,
-                width: 40,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(37, 103, 232, 1),
-                  image: DecorationImage(
+          Container(
+            height: 450,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(37, 103, 232, 1),
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: 68),
+                Center(
+                  child: Image(
                     image: AssetImage('assets/image/Vector.png'),
+                    width: 30,
                   ),
                 ),
+                SizedBox(height: 10),
+                Text(
+                  "Sign in to your",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Account",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Enter your email and password to log in",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 257,
+            left: 30,
+            child: Container(
+              padding: EdgeInsetsGeometry.all(25),
+              height: 450,
+              width: 327,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [BoxShadow(color: Colors.black)],
               ),
-            ],
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 250,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.grey.shade200),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/image/google.png',
+                              // alignment: AlignmentGeometry.center,
+                              width: 25,
+                              height: 25,
+                              fit: BoxFit.contain,
+                            ),
+                            SizedBox(width: 10),
+                            Text("Continue with Google"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    children: [
+                      Expanded(child: Divider(color: Colors.grey.shade200)),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text("Or login with"),
+                      ),
+                      Expanded(child: Divider(color: Colors.grey.shade200)),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Masukan Email",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.remove_red_eye),
+                      labelText: "Konfirmasi Password",
+                      hintText: "Masukan Konfirmasi Password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(value: false, onChanged: (value) {}),
+                          Text("Remember me"),
+                        ],
+                      ),
+
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Forgor Password",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ],
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 50,
+                      width: 300,
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have an account?"),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
